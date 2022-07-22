b7639fa64fab7d005e1b7c69796ecb46e4101bc3
smahuzaifa <huzaifasadath@gmail.com>
2022-07-22 10:50:47 +0530

    4th commit with refactored String work code to make output more neat

--- src/test/StringWork.java
+++ src/test/StringWork.java
@@ -12,22 +12,30 @@ public class StringWork {
         System.out.println(s5[1].trim());
         //This will trim any space between words
         System.out.println(s5[2]);
+        System.out.println();
         for(int i=0;i<s4.length();i++)
         {
             System.out.println(s4.charAt(i));
             //This will print each individual character of the array
         }
+        System.out.println();
         for(int i= s4.length()-1;i>=0;i--)
         {
-            System.out.println(s4.charAt(i));
+            System.out.print(s4.charAt(i)+" ");
             //This will print the string array in reverse
         }
         System.out.println();
+
         int[] a = {1,2,3,4,5,6};
+        int[] b = new int[1];
+        b[0]=2;
+        System.out.println();
+        System.out.println(b[0]);
+        System.out.println();
         for(int i=a.length-1;i>=0;i--) //Length-1 is used here because array index before its actual size must be
             // used as we start from 0
         {
-            System.out.println(a[i]);
+            System.out.print(a[i]+" ");
             //This will print the integer array in reverse
         }
     }
